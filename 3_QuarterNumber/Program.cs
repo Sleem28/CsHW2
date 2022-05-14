10aeabf42193d2ae872b092a8e9f250be63c42ff
Kiryl Pashkin <sleembelbox@gmail.com>
2022-05-14 17:37:25 +0300

    modified task3

--- 3_QuarterNumber/Program.cs
+++ 3_QuarterNumber/Program.cs
@@ -1,34 +1,36 @@
 ﻿// Задать номер четверти, показать диапазоны для возможных координат
+// Просьба за try catch не ругать!!!
 Console.Clear();
 Console.WriteLine("Покажем возможные значения координат в зависимости от номера плоскости.");
 Console.WriteLine();
 
 Console.WriteLine("Введите номер координатной плоскости: ");
-int planeNumber = int.Parse(Console.ReadLine()?? "0");
+int planeNumber = 0;
+try
+{
+ planeNumber = int.Parse(Console.ReadLine()?? "0");
+}
+catch
+{
+    Console.WriteLine("Нужно вводить только цифры!!!");  
+}
 
 if(planeNumber == 1)
-{
-    Console.WriteLine($"Возможные значения координат для плоскости с номером {planeNumber}: ");
-    Console.WriteLine("x от 0 до бесконечности");
-    Console.WriteLine("y от 0 до бесконечности");
-}
+    PrintResult("", "", planeNumber);
 else if(planeNumber == 2)
-{
-    Console.WriteLine($"Возможные значения координат для плоскости с номером {planeNumber}: ");
-    Console.WriteLine("x от 0 до минус бесконечности");
-    Console.WriteLine("y от 0 до бесконечности");
-}
+    PrintResult(" минус", "", planeNumber);
 else if(planeNumber == 3)
-{
-    Console.WriteLine($"Возможные значения координат для плоскости с номером {planeNumber}: ");
-    Console.WriteLine("x от 0 до минус бесконечности");
-    Console.WriteLine("y от 0 до минус бесконечности");
-}
+    PrintResult(" минус", " минус", planeNumber);
 else if(planeNumber == 4)
-{
-    Console.WriteLine($"Возможные значения координат для плоскости с номером {planeNumber}: ");
-    Console.WriteLine("x от 0 до бесконечности");
-    Console.WriteLine("y от 0 до минус бесконечности");
-}    
+    PrintResult("", " минус", planeNumber);    
 else
     Console.WriteLine("Введенное число не является номером плоскости");
+
+//-------------------------------Функция вывода на экран-----------------------------------------------------+
+void PrintResult(string xSign, string ySign, int planeNumber)
+{
+    Console.WriteLine($"Возможные значения координат для плоскости с номером {planeNumber}: ");
+    Console.WriteLine($"x от 0 до{xSign} бесконечности");
+    Console.WriteLine($"y от 0 до{ySign} бесконечности");
+}
+//-----------------------------------------------------------------------------------------------------------+
